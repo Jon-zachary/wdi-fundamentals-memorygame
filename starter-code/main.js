56199d9ff45c4579eb0d33d56716c2b19f401966
Jonathan Zachary <jontzachary@gmail.com>
2016-11-13 17:52:46 -0500

    created cards with js and commented out the cards in html

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,17 @@ var cardTwo="queen";
 var cardThree="king";
 var cardFour="king";
 
-if(cardOne===cardTwo){
-	alert('You found a match!')
-} else {
-	alert('Sorry, try again.')
+// if(cardOne===cardTwo){
+// 	alert('You found a match!')
+// } else {
+// 	alert('Sorry, try again.')
+// }
+var gameBoard = document.getElementById('game-board');
+var createCards = function() {
+	for(var i=0;i<4;i++){
+		var acard=document.createElement('div');
+		acard.setAttribute('class','card');
+		gameBoard.appendChild(acard);
+	}
 }
+createCards();
